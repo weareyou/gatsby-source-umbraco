5a98d59b8453cfe682bbb7e48c473f9fa1e26fba
Jesse van der Pluijm <jesse.vd.pluym@gmail.com>
2019-10-22 11:36:15 +0200

    :arrow_right: Move validate-and-prep-options util to subfolder

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,5 @@
-const validateAndPrepOptions = require('./validate-and-prep-options')
+const validateAndPrepOptions = require('./util/validate-and-prep-options')
 
 exports.sourceNodes = ({ actions }, options) => {
     options = validateAndPrepOptions(options)
 }
-
