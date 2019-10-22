4c06ccadbd71995d647c7f8f168b697c726ee5a1
Jesse van der Pluijm <jesse.vd.pluym@gmail.com>
2019-10-22 15:57:18 +0200

    :art: Format all code using prettier (npm run format)

--- util/create-and-configure-axios.js
+++ util/create-and-configure-axios.js
@@ -1,8 +1,8 @@
-const axios = require("axios");
+const axios = require("axios")
 
 module.exports = function createAndConfigureAxios(options) {
-    const config = {
-        baseURL: options.url,
-    }
-    return axios.create(config)
+  const config = {
+    baseURL: options.url,
+  }
+  return axios.create(config)
 }
