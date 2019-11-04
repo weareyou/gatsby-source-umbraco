e2ca8560091e5fa5bdf1aaaf3e24b10374604a5a
Jesse van der Pluijm <jesse.vd.pluym@gmail.com>
2019-11-04 14:40:35 +0100

    Split loading global data into it's own file

---
+++ load-global-data.js
@@ -0,0 +1,31 @@
+module.exports = async helpers => {
+  // TODO make global data route configurable
+  const { gatsby } = helpers
+
+  const fields = await fetchGlobalData(helpers)
+  const nodeMeta = createGatsbyNodeMeta(gatsby, fields)
+
+  return gatsby.actions.createNode({
+    ...nodeMeta,
+    ...fields,
+  })
+}
+
+async function fetchGlobalData({ gatsby, axios }) {
+  try {
+    const { data } = await axios.get("/asdas")
+    return data
+  } catch (error) {
+    gatsby.reporter.error(`Oeps.`, error) // TODO
+  }
+}
+
+function createGatsbyNodeMeta(gatsby, content) {
+  return {
+    id: gatsby.createNodeId(-1),
+    internal: {
+      type: "Global",
+      contentDigest: gatsby.createContentDigest(content)
+    }
+  }
+}
