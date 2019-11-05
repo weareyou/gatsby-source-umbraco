c85e711091b8058b1218d4ffba6d11aa821726d0
Jesse van der Pluijm <jesse.vd.pluym@gmail.com>
2019-11-05 10:29:52 +0100

    Add formatMessage utility

---
+++ util/formatMessage.js
@@ -0,0 +1,11 @@
+/**
+ * Format a message for logging purposes. If multiple lines are passed, they
+ * are joined by a newline character
+ * 
+ * @param {...String} lines
+ */
+module.exports = (...lines) => {
+  const prefix = "[source-umbraco] "
+  const message = lines.join("\n")
+  return prefix + message
+}
